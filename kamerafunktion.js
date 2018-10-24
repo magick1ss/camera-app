22498f12c5ffcf79af78014e7ea3eab566bc1a2d
magick1ss <44434543+magick1ss@users.noreply.github.com>
2018-10-24 15:11:36 +0200

    Add files via upload

---
+++ kamerafunktion.js
@@ -0,0 +1,11 @@
+function displayAsImage(file) {
+  var imgURL = URL.createObjectURL(file),
+      img = document.createElement('img');
+
+  img.onload = function() {
+    URL.revokeObjectURL(imgURL);
+  };
+
+  img.src = imgURL;
+  document.body.appendChild(img);
+}
