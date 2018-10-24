7a71a7f812c81f02f9a3f1c3681580c6af12f6d4
magick1ss <44434543+magick1ss@users.noreply.github.com>
2018-10-24 15:24:45 +0200

    Update kamerafunktion.js

--- kamerafunktion.js
+++ kamerafunktion.js
@@ -1,7 +1,8 @@
 var input = document.querySelector('input[type=file]');
 input.onchange = function () {
   var file = input.files[0];
-  function displayAsImage(file);
+  displayAsImage(file);
+  drawOnCanvas(file);
 };
 
 
@@ -17,4 +18,24 @@ function displayAsImage(file) {
 
   img.src = imgURL;
   document.body.appendChild(img);
+}
+function drawOnCanvas(file) {
+  var reader = new FileReader();
+
+  reader.onload = function (e) {
+    var dataURL = e.target.result,
+        c = document.querySelector('canvas'), // see Example 4
+        ctx = c.getContext('2d'),
+        img = new Image();
+
+    img.onload = function() {
+      c.width = img.width;
+      c.height = img.height;
+      ctx.drawImage(img, 0, 0);
+    };
+
+    img.src = dataURL;
+  };
+
+  reader.readAsDataURL(file);
 }
